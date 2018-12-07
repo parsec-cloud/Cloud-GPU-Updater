0cc4370e0c0b10f6147d2b2190f0c507560f223c
James Stringer <james@parsec.tv>
2018-12-07 18:20:13 +1300

    Update NVSMI.ps1

--- NVSMI.ps1
+++ NVSMI.ps1
@@ -1,7 +1,12 @@
+function GPUCurrentMode {
+$nvidiaarg = "-i 0 --query-gpu=driver_model.current --format=csv,noheader"
+$nvidiasmi = "c:\program files\nvidia corporation\nvsmi\nvidia-smi" 
+Invoke-Expression "& `"$nvidiasmi`" $nvidiaarg"}
+
 $nvidiaarg = "--query-gpu=gpu_bus_id --format=csv,noheader"
 $nvidiasmi = "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi"
 $bus_id = Invoke-Expression "& `"$nvidiasmi`" $nvidiaarg"
-if ($bus_id -eq "TCC") {
+if (GPUCurrentMode -eq "TCC") {
 $outputarg1 = "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi"
 $outputarg2 = "-g $bus_id -dm 0"
 Invoke-Expression "& `"$outputarg1`" $outputarg2"
