169c82099ff194e831459b6087d86e54911417e2
James Stringer <james@parsec.tv>
2018-12-07 17:32:29 +1300

    Update NVSMI.ps1

--- NVSMI.ps1
+++ NVSMI.ps1
@@ -1,7 +1,10 @@
 $nvidiaarg = "--query-gpu=gpu_bus_id --format=csv,noheader"
 $nvidiasmi = "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi"
 $bus_id = Invoke-Expression "& `"$nvidiasmi`" $nvidiaarg"
+if ($bus_id -eq "TCC") {
 $outputarg1 = "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi"
 $outputarg2 = "-g $bus_id -dm 0"
 Invoke-Expression "& `"$outputarg1`" $outputarg2"
 Restart-Computer
+}
+Else {Exit}
