8ed0cf80f73b63324d407f7cbca16f4c831f6ebc
James Stringer <james@parsec.tv>
2019-02-11 11:14:23 +1300

    Add support for Server 2019 (using Server 2016 Driver)

--- GPU Updater Tool.ps1
+++ GPU Updater Tool.ps1
@@ -67,6 +67,7 @@ function queryOS {
 if (($system.OS_Version -like "*Windows 10*") -eq $true) {$gpu.OSID = '57' ; $system.OS_Supported = $false}
 elseif (($system.OS_Version -like "*Windows 8.1*") -eq $true) {$gpu.OSID = "41"; $system.OS_Supported = $false}
 elseif (($system.OS_Version -like "*Server 2016*") -eq $true) {$gpu.OSID = "74"; $system.OS_Supported = $true}
+elseif (($system.OS_Version -like "*Server 2019*") -eq $true) {$gpu.OSID = "74"; $system.OS_Supported = $true}
 Else {$system.OS_Supported = $false}
 }
 
