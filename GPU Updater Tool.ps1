c59cb949c4f31117cdd288df8c6c6f3fcfa1a838
James Stringer <38541878+jamesstringerparsec@users.noreply.github.com>
2019-10-01 11:35:05 +0200

    License AWS G4 Instance

--- GPU Updater Tool.ps1
+++ GPU Updater Tool.ps1
@@ -267,6 +267,8 @@ $AWSG4SupportPage = invoke-webrequest -uri "https://aws.amazon.com/ec2/instance-
 Expand-Archive -Path $($($system.Path) + "\NVIDIA_" + $($gpu.web_driver) + ".zip") -DestinationPath $system.path
 Get-ChildItem -Path $system.path -Include *win7* -Recurse | Remove-Item
 Get-ChildItem -Path $system.path -Include *.zip* -Recurse | Remove-Item
+(New-Object System.Net.WebClient).DownloadFile("https://s3.amazonaws.com/nvidia-gaming/GridSwCert-Windows.cert", "C:\Users\Public\Documents\GridSwCert.txt")
+if((Test-RegistryValue -path 'HKLM:\SOFTWARE\NVIDIA Corporation\Global' -value 'vGamingMarketplace') -eq $true) {Set-itemproperty -path 'HKLM:\SOFTWARE\NVIDIA Corporation\Global' -Name "vGamingMarketplace" -Value "2" | Out-Null} else {new-itemproperty -path 'HKLM:\SOFTWARE\NVIDIA Corporation\Global' -Name "vGamingMarketplace" -Value "2" -PropertyType DWORD | Out-Null}
 }
 Elseif ((($gpu.supported -eq "UnOfficial")  -and ($gpu.cloudprovider -eq "Google"))-eq $true) {
 $googlestoragedriver =([xml](invoke-webrequest -uri https://storage.googleapis.com/nvidia-drivers-us-public).content).listbucketresult.contents.key  -like  "*server2016*.exe" | select -last 1
@@ -317,10 +319,6 @@ function installDriver {
 #installs driver silently with /s /n arguments provided by NVIDIA
 $DLpath = Get-ChildItem -Path $system.path -Include *exe* -Recurse | Select-Object -ExpandProperty Name
 Start-Process -FilePath "$($system.Path)\$dlpath" -ArgumentList "/s /n" -Wait 
-if((($gpu.Supported -eq "unOfficial") -and ($gpu.cloudprovider -eq "aws") -and ($gpu.Device_ID -eq "DEV_1EB8")) -eq $true){
-if((Test-RegistryValue -path 'HKLM:\SOFTWARE\NVIDIA Corporation\Global' -value 'vGamingMarketplace') -eq $true) {Set-itemproperty -path 'HKLM:\SOFTWARE\NVIDIA Corporation\Global' -Name "vGamingMarketplace" -Value "2" | Out-Null} else {new-itemproperty -path 'HKLM:\SOFTWARE\NVIDIA Corporation\Global' -Name "vGamingMarketplace" -Value "2" -PropertyType DWORD | Out-Null}
-}
-Else{}
 }
 
 #setting up arrays below
