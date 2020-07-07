fbe7a34a057870fa509ef478cba2fafb0b46648f
James Stringer <38541878+jamesstringerparsec@users.noreply.github.com>
2020-07-07 17:10:21 +0200

    Update URLs

--- GPUUpdaterTool.ps1
+++ GPUUpdaterTool.ps1
@@ -280,7 +280,7 @@ Type Y to continue, or N to exit.
 
 function webName {
     #Gets the unknown GPU name from a csv based on a deviceID found in the installedgpuid function
-    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/jamesstringerparsec/Cloud-GPU-Updater/master/Additional%20Files/GPUID.csv", $($system.Path + "\GPUID.CSV")) 
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/Additional%20Files/GPUID.csv", $($system.Path + "\GPUID.CSV")) 
     Import-Csv "$($system.path)\GPUID.csv" -Delimiter ',' | Where-Object DeviceID -like *$($gpu.Device_ID)* | Select-Object -ExpandProperty GPUName
 }
 
@@ -449,7 +449,7 @@ function Test-RegistryValue {
 
 function DisableSecondMonitor {
     #downloads script to set GPU to WDDM if required
-    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/jamesstringerparsec/Cloud-GPU-Updater/master/Additional%20Files/DisableSecondMonitor.ps1", $($system.Path) + "\DisableSecondMonitor.ps1") 
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/Additional%20Files/DisableSecondMonitor.ps1", $($system.Path) + "\DisableSecondMonitor.ps1") 
     Unblock-File -Path "$($system.Path)\DisableSecondMonitor.ps1"
 }
 
@@ -553,7 +553,7 @@ function rebootLogic {
 
 function setnvsmi {
     #downloads script to set GPU to WDDM if required
-    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/jamesstringerparsec/Cloud-GPU-Updater/master/Additional%20Files/NVSMI.ps1", $($system.Path) + "\NVSMI.ps1") 
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/Additional%20Files/NVSMI.ps1", $($system.Path) + "\NVSMI.ps1") 
     Unblock-File -Path "$($system.Path)\NVSMI.ps1"
 }
 
