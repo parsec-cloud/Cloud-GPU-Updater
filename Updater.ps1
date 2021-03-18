c4fdab7e6d6eef32829256ad1a89af3b2c10a3cc
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2021-03-18 16:43:01 +0100

    Fixed 404 error
    On line 3, URL was with spaces but the file itself not having spaces, so I just replaced with actual URL

--- Updater.ps1
+++ Updater.ps1
@@ -1,5 +1,5 @@
 ﻿$PSScriptRoot
 $installedversion = (Get-Content  -Path '.\GPU Updater Tool.ps1')[0].Split('=')[1]
-$onlineversion = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/GPU%20Updater%20Tool.ps1").Content.Substring(10,3)
+$onlineversion = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/GPUUpdaterTool.ps1").Content.Substring(10,3)
 if ($onlineversion -gt $installedversion){"Update"} Else {"No Update"}
 #https://github.com/jamesstringerparsec/Cloud-GPU-Updater/archive/dev.zip
